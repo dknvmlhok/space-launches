8c769c983ac5ddca09be1db8e35dfed44f27592f
Dominik Kohlman <dominik.kohlman@gmail.com>
2021-02-25 23:36:50 +0100

    Added View extension for cacheable Image

--- Space Launches/Utils/View+Ext.swift
+++ Space Launches/Utils/View+Ext.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 extension View {
 
@@ -15,5 +16,29 @@ extension View {
 
     func hideKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+
+    func getCacheableImage(
+        from url: URL?,
+        of size: CGSize,
+        placeholderImage: UIImage
+    ) -> KFImage {
+        KFImage(url)
+            .placeholder {
+                Image(uiImage: UIImage(named: "rocket")!)
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundColor(.white)
+                    .frame(
+                        width: size.width,
+                        height: size.height,
+                        alignment: .center
+                    )
+            }
+            .downsampling(size: size)
+            .retry(maxCount: 3, interval: .seconds(5))
+            .cacheOriginalImage()
+            .fade(duration: 0.25)
+            .resizable()
     }
 }
